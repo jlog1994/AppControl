ed91f1a3dbe15804ee6211ffa378f04d6ba576c9
Eugenio Bautista <egenio.21@gmail>
2015-11-23 16:37:34 -0600

    Modificando logeo

--- index.php
+++ index.php
@@ -20,24 +20,26 @@ if (@$_SESSION["name"]) {
 	<script src="_assets/js/index.js"></script>
 	<script src="js/jquery.mobile-1.4.5.min.js"></script>
 </head> 
-<body>
-<div data-role="page"> 
-	<div data-role="header">
-		<h1>Inicio de Session</h1>
-		<a href="index.php" data-icon="home" data-iconpos="notext" data-direction="reverse">Home</a>
-		<a href="../nav.html" data-icon="search" data-iconpos="notext" data-rel="dialog" data-transition="fade">Search</a>		
-	</div>	
-	<div data-role="content">
-		 <form name="form1" action="index.php" method="POST">
+<body> 
+<div data-role="page" class="jqm-demos jqm-home">
+	<div data-role="header" class="jqm-header">
+		<h2><a href="http://localhost/App/index.php"><img src="_assets/img/logo.png" alt="jQuery Mobile"></a></h2>
+		<a href="#" class="jqm-navmenu-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-bars ui-nodisc-icon ui-alt-icon ui-btn-left">Menu</a>
+		<a href="#" class="jqm-search-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-search ui-nodisc-icon ui-alt-icon ui-btn-right">Search</a>
+	</div><!-- /header -->
+
+	<div role="main" class="ui-content jqm-content">
+	
+	<h1>Autentificación del Usuario</h1>
+		 <form name="form1"  action="index.php" method="POST">
           <label for="basic"/>Usuario:</label>
           <input type="text" name="name"  value="" required />
     	  <label for="basic"/>Password:</label>
           <input type="password"  name="password"  value="" required />
 		  <input type="hidden" name="fn" value="20" />
 		  <input type="submit" value="Autentificación" />
-		 </form>	
-		 </div>
-		  <?php
+		 </form>
+	 	<?php
 			@$usuario=$_REQUEST['name'];
 			@$password=md5($_REQUEST['password']);
 			if (isset($usuario)and isset($password)){
@@ -55,10 +57,21 @@ if (@$_SESSION["name"]) {
     			}
 			}		
 		?>
-		<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
+	</div><!-- /content -->
+<div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
+	  <ul class="jqm-list ui-alt-icon ui-nodisc-icon">
+		<li data-filtertext="demos homepage" data-icon="home"><a href="home2.html" target="cont">Home</a></li>
+		<li data-filtertext="demos homepage" data-icon="user"><a href="user.php" target="cont">Crear cuenta</a></li>
+		<li data-filtertext="demos homepage" data-icon="user"><a href="contacto.php" target="cont">Contacto</a></li>
+		<iframe width="100%" height="600px" src="home2.html" name="cont" frameborder="0" padding="0px" allowfullscreen></iframe>
+	 </ul>
+</div><!-- /panel -->
+
+<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
 		<p>Sistema de Venta  version Demos<span class="jqm-version"></span></p>
 		<p>Copyright 2015. Instituto Tecnologico de Tlaxiaco</p>
 </div><!-- /footer -->
+	
 </div>
 </body>
 </html>
