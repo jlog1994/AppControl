f0df6b7e8df9f32af8aafd206e877e499be36370
Eugenio Bautista <egenio.21@gmail>
2015-11-24 14:51:49 -0600

    Modificando Registro

--- user.php
+++ user.php
@@ -8,15 +8,11 @@
 	<script src="js/jquery.mobile-1.4.5.min.js"></script>
 	
 	
-	<h1>Datos del Usuario</h1>
+	<h1>Registro Usuario</h1>
 	<form name="formreg" target="cont"action="insertar.php" method="POST">
         <div style="padding:10px 20px;">
-            <h3>Registro</h3>
-          <label for="basic">Nombre:</label>
           <input type="text" name="name" id="basic"  placeholder="Ingrese su Usuario" value="" required />
-    	  <label for="basic">Email:</label>
           <input type="email" name="email" id="basic"  placeholder="Ingrese su Correo" value="" required />
-			<label for="pw" >Password:</label>
 			<input  id="pw" name="password" data-theme="a" type="password"  required >
             <input type="hidden" name="fn" value="12"/>
             <button type="submit" data-theme="b" data-icon="check">Registrarse</button>
